ca4a9b93537c119f2436fdab9d9fe29add8a0763
Yeukai M. Guri <guriyeukaim@gmail.com>
2026-08-01 15:09:37 +0100

    Updated Create new VPC

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "aws_instance" "blog"{
 }
 
 module "blog_sg" {
-  source  = "terraform-aws-modules/security-groups/aws"
+  source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
   vpc_id  = module.blog_vpc.vpc_id
