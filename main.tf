c64315a8ad62edb70623fac4f678cfbc56096f1e
Yeukai M. Guri <guriyeukaim@gmail.com>
2026-08-01 16:08:40 +0100

    autoscaling

--- main.tf
+++ main.tf
@@ -38,19 +38,7 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog"{
-  ami               = data.aws_ami.app_ami.id
-  instance_type     = var.instance_type
-  vpc_security_group_ids =[module.blog_sg.security_group_id]
 
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name= "Learning Terraform"
-
-  }
-
-}
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
@@ -105,3 +93,27 @@ resource "aws_lb_target_group_attachment" "blog" {
   target_id        = aws_instance.blog.id
   port             = 80
 }
+
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "9.2.1"
+  name = "blog"
+
+  min_size = 1
+  max_siz3 = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+
+  launch_template_name = "blog"
+  security_groups = [module.blog_sg.security_group_id]
+  instance_type     = var.instance_type
+  image_id               = data.aws_ami.app_ami.id
+
+
+  traffic_source_attachments = {
+    blog_alb = {
+      traffic_source_identifier = aws_lb_target_group.arn
+    }
+  }
+}
+
